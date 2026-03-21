8563bdc747038d539bcb1330cd7959526aa57826
Nestor Komarnitskiy <nestorkomarnitskiy@gmail.com>
2026-03-21 14:17:48 +0200

    create test project

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import svelteLogo from "./assets/svelte.svg";
+    import viteLogo from "./assets/vite.svg";
+    import heroImg from "./assets/hero.png";
+    import Counter from "./lib/Counter.svelte";
+    import "./app.css";
+    import SideBar from "./lib/SideBar.svelte";
+    import Main from "./lib/Main.svelte";
+</script>
+
+<div class="bg-gray-100">
+    <div class="container mx-auto py-8">
+        <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
+            <SideBar name={"Nestor"} />
+            <Main />
+        </div>
+    </div>
+</div>
